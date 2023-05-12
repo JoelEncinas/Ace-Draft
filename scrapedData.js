5147b73eb6602b2fb3e1f102862b0422cf549737
JoelEncinas <joelencinasmartin@gmail.com>
2023-05-13 00:24:45 +0200

    generated scraped data

---
+++ scrapedData.js
@@ -0,0 +1,508 @@
+const data = [
+  {
+    "champion": "Alistar",
+    "winRate": "49.81%"
+  },
+  {
+    "champion": "Annie",
+    "winRate": "51.81%"
+  },
+  {
+    "champion": "Ashe",
+    "winRate": "49.65%"
+  },
+  {
+    "champion": "Fiddlesticks",
+    "winRate": "50.62%"
+  },
+  {
+    "champion": "Jax",
+    "winRate": "51.47%"
+  },
+  {
+    "champion": "Kayle",
+    "winRate": "50.01%"
+  },
+  {
+    "champion": "Master Yi",
+    "winRate": "49.39%"
+  },
+  {
+    "champion": "Morgana",
+    "winRate": "47.9%"
+  },
+  {
+    "champion": "Nunu & Willump",
+    "winRate": ""
+  },
+  {
+    "champion": "Ryze",
+    "winRate": "47.84%"
+  },
+  {
+    "champion": "Sion",
+    "winRate": "48.14%"
+  },
+  {
+    "champion": "Sivir",
+    "winRate": "48.99%"
+  },
+  {
+    "champion": "Soraka",
+    "winRate": "49.55%"
+  },
+  {
+    "champion": "Teemo",
+    "winRate": "48.64%"
+  },
+  {
+    "champion": "Tristana",
+    "winRate": "49.9%"
+  },
+  {
+    "champion": "Twisted Fate",
+    "winRate": "48.73%"
+  },
+  {
+    "champion": "Warwick",
+    "winRate": "49.27%"
+  },
+  {
+    "champion": "Veigar",
+    "winRate": "45.61%"
+  },
+  {
+    "champion": "Evelynn",
+    "winRate": "52.02%"
+  },
+  {
+    "champion": "Gragas",
+    "winRate": "51.99%"
+  },
+  {
+    "champion": "Corki",
+    "winRate": "48.72%"
+  },
+  {
+    "champion": "Amumu",
+    "winRate": "48.39%"
+  },
+  {
+    "champion": "Rammus",
+    "winRate": "49.92%"
+  },
+  {
+    "champion": "Anivia",
+    "winRate": "52.33%"
+  },
+  {
+    "champion": "Shaco",
+    "winRate": "50.98%"
+  },
+  {
+    "champion": "Dr. Mundo",
+    "winRate": "50.19%"
+  },
+  {
+    "champion": "Singed",
+    "winRate": "52.92%"
+  },
+  {
+    "champion": "Heimerdinger",
+    "winRate": "50.86%"
+  },
+  {
+    "champion": "Kassadin",
+    "winRate": "49.32%"
+  },
+  {
+    "champion": "Zilean",
+    "winRate": "49.74%"
+  },
+  {
+    "champion": "Olaf",
+    "winRate": "51.33%"
+  },
+  {
+    "champion": "Ezreal",
+    "winRate": "49.69%"
+  },
+  {
+    "champion": "Taric",
+    "winRate": "51.7%"
+  },
+  {
+    "champion": "Malphite",
+    "winRate": "50.32%"
+  },
+  {
+    "champion": "Renekton",
+    "winRate": "47.34%"
+  },
+  {
+    "champion": "Katarina",
+    "winRate": "50.22%"
+  },
+  {
+    "champion": "Jarvan IV",
+    "winRate": "50.51%"
+  },
+  {
+    "champion": "Elise",
+    "winRate": "51.71%"
+  },
+  {
+    "champion": "Orianna",
+    "winRate": "50.42%"
+  },
+  {
+    "champion": "Monkey King",
+    "winRate": ""
+  },
+  {
+    "champion": "Nautilus",
+    "winRate": "50.08%"
+  },
+  {
+    "champion": "Viktor",
+    "winRate": "48.62%"
+  },
+  {
+    "champion": "Sejuani",
+    "winRate": "46.7%"
+  },
+  {
+    "champion": "Fiora",
+    "winRate": "51.05%"
+  },
+  {
+    "champion": "Volibear",
+    "winRate": "49.55%"
+  },
+  {
+    "champion": "Ahri",
+    "winRate": "50.91%"
+  },
+  {
+    "champion": "Vayne",
+    "winRate": "49.57%"
+  },
+  {
+    "champion": "Rumble",
+    "winRate": "49.23%"
+  },
+  {
+    "champion": "Cassiopeia",
+    "winRate": "51.39%"
+  },
+  {
+    "champion": "Skarner",
+    "winRate": "52.68%"
+  },
+  {
+    "champion": "Ziggs",
+    "winRate": "47.31%"
+  },
+  {
+    "champion": "Nautilus",
+    "winRate": "50.08%"
+  },
+  {
+    "champion": "Hecarim",
+    "winRate": "49.62%"
+  },
+  {
+    "champion": "Lulu",
+    "winRate": "47.47%"
+  },
+  {
+    "champion": "Draven",
+    "winRate": "52.23%"
+  },
+  {
+    "champion": "Syndra",
+    "winRate": "49.57%"
+  },
+  {
+    "champion": "Rengar",
+    "winRate": "53.82%"
+  },
+  {
+    "champion": "KhaZix",
+    "winRate": "51.91%"
+  },
+  {
+    "champion": "Nami",
+    "winRate": "48.2%"
+  },
+  {
+    "champion": "Zyra",
+    "winRate": "49.9%"
+  },
+  {
+    "champion": "Thresh",
+    "winRate": "50.35%"
+  },
+  {
+    "champion": "Quinn",
+    "winRate": "52.64%"
+  },
+  {
+    "champion": "Zac",
+    "winRate": "51.57%"
+  },
+  {
+    "champion": "Lissandra",
+    "winRate": "48.96%"
+  },
+  {
+    "champion": "Aatrox",
+    "winRate": "49.27%"
+  },
+  {
+    "champion": "Lucian",
+    "winRate": "49.44%"
+  },
+  {
+    "champion": "Jinx",
+    "winRate": "49.78%"
+  },
+  {
+    "champion": "Yasuo",
+    "winRate": "49.65%"
+  },
+  {
+    "champion": "VelKoz",
+    "winRate": "51.28%"
+  },
+  {
+    "champion": "Braum",
+    "winRate": "49.22%"
+  },
+  {
+    "champion": "Gnar",
+    "winRate": "48.69%"
+  },
+  {
+    "champion": "Azir",
+    "winRate": "49.81%"
+  },
+  {
+    "champion": "Kalista",
+    "winRate": "51.8%"
+  },
+  {
+    "champion": "RekSai",
+    "winRate": "55.13%"
+  },
+  {
+    "champion": "Ekko",
+    "winRate": "49.75%"
+  },
+  {
+    "champion": "Bard",
+    "winRate": "51.17%"
+  },
+  {
+    "champion": "Tahm Kench",
+    "winRate": "51%"
+  },
+  {
+    "champion": "Jhin",
+    "winRate": "50.41%"
+  },
+  {
+    "champion": "Kindred",
+    "winRate": "51.62%"
+  },
+  {
+    "champion": "Illaoi",
+    "winRate": "48.06%"
+  },
+  {
+    "champion": "Yorick",
+    "winRate": "47.13%"
+  },
+  {
+    "champion": "Taliyah",
+    "winRate": "52.17%"
+  },
+  {
+    "champion": "Camille",
+    "winRate": "50.47%"
+  },
+  {
+    "champion": "Kled",
+    "winRate": "51.96%"
+  },
+  {
+    "champion": "Ivern",
+    "winRate": "48.44%"
+  },
+  {
+    "champion": "Rakan",
+    "winRate": "52.54%"
+  },
+  {
+    "champion": "Xayah",
+    "winRate": "51.18%"
+  },
+  {
+    "champion": "Kayn",
+    "winRate": "48.87%"
+  },
+  {
+    "champion": "Ornn",
+    "winRate": "51.01%"
+  },
+  {
+    "champion": "Zoe",
+    "winRate": "53.18%"
+  },
+  {
+    "champion": "Pyke",
+    "winRate": "51.17%"
+  },
+  {
+    "champion": "Neeko",
+    "winRate": "51.73%"
+  },
+  {
+    "champion": "Sylas",
+    "winRate": "49.42%"
+  },
+  {
+    "champion": "Yuumi",
+    "winRate": "46.43%"
+  },
+  {
+    "champion": "Qiyana",
+    "winRate": "50.16%"
+  },
+  {
+    "champion": "Senna",
+    "winRate": "51.81%"
+  },
+  {
+    "champion": "Aphelios",
+    "winRate": "47.98%"
+  },
+  {
+    "champion": "Sett",
+    "winRate": "48.88%"
+  },
+  {
+    "champion": "Lillia",
+    "winRate": "49.61%"
+  },
+  {
+    "champion": "Yone",
+    "winRate": "48.51%"
+  },
+  {
+    "champion": "Samira",
+    "winRate": "50.69%"
+  },
+  {
+    "champion": "Seraphine",
+    "winRate": "49.96%"
+  },
+  {
+    "champion": "Rell",
+    "winRate": "52.07%"
+  },
+  {
+    "champion": "Viego",
+    "winRate": "48.35%"
+  },
+  {
+    "champion": "Gwen",
+    "winRate": "49.78%"
+  },
+  {
+    "champion": "Akshan",
+    "winRate": "53.08%"
+  },
+  {
+    "champion": "Yuumi",
+    "winRate": "46.43%"
+  },
+  {
+    "champion": "Qiyana",
+    "winRate": "50.16%"
+  },
+  {
+    "champion": "Senna",
+    "winRate": "51.81%"
+  },
+  {
+    "champion": "Aphelios",
+    "winRate": "47.98%"
+  },
+  {
+    "champion": "Sett",
+    "winRate": "48.88%"
+  },
+  {
+    "champion": "Lillia",
+    "winRate": "49.61%"
+  },
+  {
+    "champion": "Yone",
+    "winRate": "48.51%"
+  },
+  {
+    "champion": "Samira",
+    "winRate": "50.69%"
+  },
+  {
+    "champion": "Seraphine",
+    "winRate": "49.96%"
+  },
+  {
+    "champion": "Rell",
+    "winRate": "52.07%"
+  },
+  {
+    "champion": "Viego",
+    "winRate": "48.35%"
+  },
+  {
+    "champion": "Gwen",
+    "winRate": "49.78%"
+  },
+  {
+    "champion": "Akshan",
+    "winRate": "53.08%"
+  },
+  {
+    "champion": "Vex",
+    "winRate": "51.47%"
+  },
+  {
+    "champion": "Zeri",
+    "winRate": "50.32%"
+  },
+  {
+    "champion": "RenataGlasc",
+    "winRate": ""
+  },
+  {
+    "champion": "BelVeth",
+    "winRate": "51.87%"
+  },
+  {
+    "champion": "Nilah",
+    "winRate": "51.8%"
+  },
+  {
+    "champion": "KSante",
+    "winRate": "48.71%"
+  },
+  {
+    "champion": "Milio",
+    "winRate": "50.79%"
+  }
+];
+
+module.exports = data;
