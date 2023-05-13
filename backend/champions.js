b7c95654b612abdeb6922b572d0b22e00031691b
JoelEncinas <joelencinasmartin@gmail.com>
2023-05-13 02:18:16 +0200

    removed comments

--- backend/champions.js
+++ backend/champions.js
@@ -1,123 +1,110 @@
 const champions = [
-  "Alistar", // February 21, 2009
-  "Annie", // February 21, 2009
-  "Ashe", // February 21, 2009
-  "Fiddlesticks", // February 21, 2009
-  "Jax", // February 21, 2009
-  "Kayle", // February 21, 2009
-  "Master Yi", // February 21, 2009
-  "Morgana", // February 21, 2009
-  "Nunu", // February 21, 2009
-  "Ryze", // February 21, 2009
-  "Sion", // February 21, 2009
-  "Sivir", // February 21, 2009
-  "Soraka", // February 21, 2009
-  "Teemo", // February 21, 2009
-  "Tristana", // February 21, 2009
-  "Twisted Fate", // February 21, 2009
-  "Warwick", // February 21, 2009
-  "Veigar", // July 24, 2009
-  "Evelynn", // May 1, 2009
-  "Gragas", // February 2, 2010
-  "Corki", // September 19, 2009
-  "Amumu", // June 26, 2009
-  "Rammus", // July 10, 2009
-  "Anivia", // July 10, 2009
-  "Shaco", // October 10, 2009
-  "Dr. Mundo", // September 2, 2009
-  "Singed", // April 18, 2009
-  "Heimerdinger", // October 10, 2009
-  "Kassadin", // August 7, 2009
-  "Zilean", // April 18, 2009
-  "Olaf", // June 9, 2010
-  "Ezreal", // March 16, 2010
-  "Taric", // August 19, 2009
-  "Malphite", // June 10, 2009
-  "Renekton", // January 18, 2011
-  "Katarina", // September 19, 2009
-  "Jarvan IV", // March 1, 2011
-  "Elise", // October 26, 2012
-  "Orianna", // June 1, 2011
-  "Wukong", // July 26, 2011
-  "Nautilus", // February 14, 2012
-  "Viktor", // December 29, 2011
-  "Sejuani", // January 17, 2012
-  "Fiora", // February 29, 2012
-  "Volibear", // November 29, 2011
-  "Ahri", // December 14, 2011
-  "Vayne", // May 10, 2011
-  "Rumble", // April 26, 2011
-  "Cassiopeia", // December 14, 2010
-  "Skarner", // August 9, 2011
-  "Ziggs", // February 1, 2012
-  "Nautilus", // February 14, 2012
-  "Hecarim", // April 18, 2012
-  "Lulu", // March 20, 2012
-  "Draven", // June 6, 2012
-  "Syndra", // September 13, 2012
-  "Rengar", // August 21, 2012
-  "KhaZix", // September 27, 2012
-  "Nami", // December 7, 2012
-  "Zyra", // July 24, 2012
-  "Thresh", // January 23, 2013
-  "Quinn", // March 1, 2013
-  "Zac", // March 29, 2013
-  "Lissandra", // April 30, 2013
-  "Aatrox", // June 13, 2013
-  "Lucian", // August 22, 2013
-  "Jinx", // October 10, 2013
-  "Yasuo", // December 13, 2013
-  "VelKoz", // February 27, 2014
-  "Braum", // May 12, 2014
-  "Gnar", // August 14, 2014
-  "Azir", // September 16, 2014
-  "Kalista", // November 20, 2014
-  "RekSai", // December 11, 2014
-  "Ekko", // May 28, 2015
-  "Bard", // March 12, 2015
-  "Tahm Kench", // July 9, 2015
-  "Jhin", // February 1, 2016
-  "Kindred", // October 14, 2015
-  "Illaoi", // November 24, 2015
-  "Yorick", // August 23, 2016
-  "Taliyah", // May 18, 2016
-  "Camille", // December 10, 2016
-  "Kled", // August 10, 2016
-  "Ivern", // October 5, 2016
-  "Rakan", // April 19, 2017
-  "Xayah", // April 19, 2017
-  "Kayn", // July 12, 2017
-  "Ornn", // August 23, 2017
-  "Zoe", // November 21, 2017
-  "Pyke", // May 31, 2018
-  "Neeko", // December 5, 2018
-  "Sylas", // January 25, 2019
-  "Yuumi", // May 14, 2019
-  "Qiyana", // June 28, 2019
-  "Senna", // November 10, 2019
-  "Aphelios", // December 11, 2019
-  "Sett", // January 14, 2020
-  "Lillia", // July 22, 2020
-  "Yone", // August 6, 2020
-  "Samira", // September 21, 2020
-  "Seraphine", // October 29, 2020
-  "Rell", // December 10, 2020
-  "Viego", // January 22, 2021
-  "Gwen", // April 15, 2021
-  "Akshan", // July 21, 2021
-  "Yuumi", // May 14, 2019 (Rerelease)
-  "Qiyana", // June 28, 2019 (Rerelease)
-  "Senna", // November 10, 2019 (Rerelease)
-  "Aphelios", // December 11, 2019 (Rerelease)
-  "Sett", // January 14, 2020 (Rerelease)
-  "Lillia", // July 22, 2020 (Rerelease)
-  "Yone", // August 6, 2020 (Rerelease)
-  "Samira", // September 21, 2020 (Rerelease)
-  "Seraphine", // October 29, 2020 (Rerelease)
-  "Rell", // December 10, 2020 (Rerelease)
-  "Viego", // January 22, 2021 (Rerelease)
-  "Gwen", // April 15, 2021 (Rerelease)
-  "Akshan", // July 21, 2021 (Rerelease)
+  "Alistar",
+  "Annie",
+  "Ashe",
+  "Fiddlesticks",
+  "Jax",
+  "Kayle",
+  "Master Yi",
+  "Morgana",
+  "Nunu",
+  "Ryze",
+  "Sion",
+  "Sivir",
+  "Soraka",
+  "Teemo",
+  "Tristana",
+  "Twisted Fate",
+  "Warwick",
+  "Veigar",
+  "Evelynn",
+  "Gragas",
+  "Corki",
+  "Amumu",
+  "Rammus",
+  "Anivia",
+  "Shaco",
+  "Dr. Mundo",
+  "Singed",
+  "Heimerdinger",
+  "Kassadin",
+  "Zilean",
+  "Olaf",
+  "Ezreal",
+  "Taric",
+  "Malphite",
+  "Renekton",
+  "Katarina",
+  "Jarvan IV",
+  "Elise",
+  "Orianna",
+  "Wukong",
+  "Nautilus",
+  "Viktor",
+  "Sejuani",
+  "Fiora",
+  "Volibear",
+  "Ahri",
+  "Vayne",
+  "Rumble",
+  "Cassiopeia",
+  "Skarner",
+  "Ziggs",
+  "Nautilus",
+  "Hecarim",
+  "Lulu",
+  "Draven",
+  "Syndra",
+  "Rengar",
+  "KhaZix",
+  "Nami",
+  "Zyra",
+  "Thresh",
+  "Quinn",
+  "Zac",
+  "Lissandra",
+  "Aatrox",
+  "Lucian",
+  "Jinx",
+  "Yasuo",
+  "VelKoz",
+  "Braum",
+  "Gnar",
+  "Azir",
+  "Kalista",
+  "RekSai",
+  "Ekko",
+  "Bard",
+  "Tahm Kench",
+  "Jhin",
+  "Kindred",
+  "Illaoi",
+  "Yorick",
+  "Taliyah",
+  "Camille",
+  "Kled",
+  "Ivern",
+  "Rakan",
+  "Xayah",
+  "Kayn",
+  "Ornn",
+  "Zoe",
+  "Pyke",
+  "Neeko",
+  "Sylas",
+  "Yuumi",
+  "Qiyana",
+  "Senna",
+  "Aphelios",
+  "Sett",
+  "Lillia",
+  "Yone",
+  "Samira",
+  "Seraphine",
+  "Rell",
+  "Viego",
+  "Gwen",
+  "Akshan",
   "Vex",
   "Zeri",
   "Renata",
