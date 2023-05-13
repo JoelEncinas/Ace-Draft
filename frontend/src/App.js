4debd91fff57a3183f094fc2f2a23ea7ea928edc
JoelEncinas <joelencinasmartin@gmail.com>
2023-05-13 03:43:03 +0200

    improve search by removing . ' and white spaces from names

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,25 +9,33 @@ function App() {
   };
 
   const filteredChampions = champions.filter((champion) =>
-    champion.toLowerCase().includes(filter)
+    champion
+      .toLowerCase()
+      .replace(/[.'\s]/g, "")
+      .includes(filter)
   );
 
   return (
     <div className="App">
-      <input
-        type="text"
-        value={filter}
-        onChange={handleFilterChange}
-        placeholder="Filter champions"
-      />
-      {filteredChampions.map((champion) => (
-        <img
-          key={champion}
-          src={`./champion_images/${champion}.png`}
-          alt={champion}
-          style={{ width: 50 }}
+      <div className="text-center">
+        <input
+          type="text"
+          value={filter}
+          onChange={handleFilterChange}
+          placeholder="Search"
         />
-      ))}
+      </div>
+      <div className="container">
+        {filteredChampions.map((champion) => (
+          <img
+            className="m-1"
+            key={champion}
+            src={`./champion_images/${champion}.png`}
+            alt={champion}
+            style={{ width: 75 }}
+          />
+        ))}
+      </div>
     </div>
   );
 }
