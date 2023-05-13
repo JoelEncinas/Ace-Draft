32c66b04e1950c36b0d81a65780f65b85e9fd230
JoelEncinas <joelencinasmartin@gmail.com>
2023-05-13 04:25:17 +0200

    more styling and hightlight on click

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,14 @@ import "./App.css";
 
 function App() {
   const [filter, setFilter] = useState("");
+  const [clickedChampion, setClickedChampion] = useState("");
+
+  const temp = ["", "", "", "", ""];
+
+  const handleChampionClick = (champion) => {
+    console.log(champion);
+    setClickedChampion(champion);
+  };
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value.toLowerCase());
@@ -21,8 +29,49 @@ function App() {
     <div className="App">
       <div className="container">
         <div className="row">
-          <div className="col-sm-2">
+          <div className="col-sm-6">
             <div className="p-2 blue">Blue Side</div>
+            {temp.map((item, id) => (
+              <img
+                className="m-1"
+                key={id}
+                src={`./champion_images/1.png`}
+                alt="no champion selected"
+                style={{ width: 75 }}
+              />
+            ))}
+          </div>
+          <div className="col-sm-6">
+            <div className="p-2 red text-right">Red Side</div>
+            <div className="text-right">
+              <div>
+                {temp.map((item, id) => (
+                  <img
+                    className="m-1"
+                    key={id}
+                    src={`./champion_images/1.png`}
+                    alt="no champion selected"
+                    style={{ width: 75 }}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-2">
+            <div>
+              {" "}
+              {temp.map((item, id) => (
+                <img
+                  className="m-1"
+                  key={id}
+                  src={`./champion_images/1.png`}
+                  alt="no champion selected"
+                  style={{ width: 75 }}
+                />
+              ))}
+            </div>
           </div>
           <div className="col-sm-8">
             <div className="p-2">
@@ -34,21 +83,36 @@ function App() {
                   placeholder="Search"
                 />
               </div>
-              <div className="container d-flex flex-wrap justify-content-center draft">
-                {filteredChampions.map((champion) => (
-                  <img
-                    className="m-1"
-                    key={champion}
-                    src={`./champion_images/${champion}.png`}
-                    alt={champion}
-                    style={{ width: 75 }}
-                  />
-                ))}
+              <div className="container draft">
+                <div>
+                  {filteredChampions.map((champion) => (
+                    <img
+                      className={`m-1 ${
+                        clickedChampion === champion ? "active" : ""
+                      }`}
+                      key={champion}
+                      src={`./champion_images/${champion}.png`}
+                      alt={champion}
+                      style={{ height: 75 }}
+                      onClick={() => handleChampionClick(champion)}
+                    />
+                  ))}
+                </div>
               </div>
             </div>
           </div>
           <div className="col-sm-2">
-            <div className="p-2 red">Red Side</div>
+            <div className="text-right">
+              {temp.map((item, id) => (
+                <img
+                  className="m-1"
+                  key={id}
+                  src={`./champion_images/1.png`}
+                  alt="no champion selected"
+                  style={{ width: 75 }}
+                />
+              ))}
+            </div>
           </div>
         </div>
       </div>
