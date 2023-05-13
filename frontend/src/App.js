c4ed10750956079c808e77aad7b6e22b27dfa17b
JoelEncinas <joelencinasmartin@gmail.com>
2023-05-13 04:00:28 +0200

    style draft container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import champions from "./utils/champions";
 
+import "./App.css";
+
 function App() {
   const [filter, setFilter] = useState("");
 
@@ -17,24 +19,38 @@ function App() {
 
   return (
     <div className="App">
-      <div className="text-center">
-        <input
-          type="text"
-          value={filter}
-          onChange={handleFilterChange}
-          placeholder="Search"
-        />
-      </div>
       <div className="container">
-        {filteredChampions.map((champion) => (
-          <img
-            className="m-1"
-            key={champion}
-            src={`./champion_images/${champion}.png`}
-            alt={champion}
-            style={{ width: 75 }}
-          />
-        ))}
+        <div className="row">
+          <div className="col-sm-2">
+            <div className="p-2 blue">Blue Side</div>
+          </div>
+          <div className="col-sm-8">
+            <div className="p-2">
+              <div className="text-center mb-3">
+                <input
+                  type="text"
+                  value={filter}
+                  onChange={handleFilterChange}
+                  placeholder="Search"
+                />
+              </div>
+              <div className="container d-flex flex-wrap justify-content-center draft">
+                {filteredChampions.map((champion) => (
+                  <img
+                    className="m-1"
+                    key={champion}
+                    src={`./champion_images/${champion}.png`}
+                    alt={champion}
+                    style={{ width: 75 }}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="col-sm-2">
+            <div className="p-2 red">Red Side</div>
+          </div>
+        </div>
       </div>
     </div>
   );
