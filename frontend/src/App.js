4fb61d0ba259d9606dcb2368bdeaa67c4d141449
JoelEncinas <joelencinasmartin@gmail.com>
2023-05-13 03:33:58 +0200

    updated champion lists and implemented champion filter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,33 @@
-import championsDD from "./utils/champions";
+import React, { useState } from "react";
+import champions from "./utils/champions";
 
 function App() {
+  const [filter, setFilter] = useState("");
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value.toLowerCase());
+  };
+
+  const filteredChampions = champions.filter((champion) =>
+    champion.toLowerCase().includes(filter)
+  );
+
   return (
     <div className="App">
-      {championsDD.map((champion) => {
-        const championLowerCase = champion.toLowerCase();
-        return (
-          <img
-            key={champion}
-            src={`./champion_images/${champion}.png`}
-            alt={`${champion}`}
-            style={{width: 50}}
-          />
-        );
-      })}
+      <input
+        type="text"
+        value={filter}
+        onChange={handleFilterChange}
+        placeholder="Filter champions"
+      />
+      {filteredChampions.map((champion) => (
+        <img
+          key={champion}
+          src={`./champion_images/${champion}.png`}
+          alt={champion}
+          style={{ width: 50 }}
+        />
+      ))}
     </div>
   );
 }
