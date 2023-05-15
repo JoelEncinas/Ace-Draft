9cbbcce8fe3b04a0f5e0ed0d898b2dba64a3df9e
JoelEncinas <joelencinasmartin@gmail.com>
2023-05-15 21:44:17 +0200

    added brand icon and favicons

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,13 +5,13 @@ const NavBar = () => {
     <nav id="top" className="navbar navbar-dark bg-dark px-3 mb-2">
       <div className="container">
         <Link to="/" className="navbar-brand text-center">
-          Ace Draft
+          <img className="brand-img" src="./logo192.png" alt="brand"></img>
         </Link>
 
         <ul className="navbar-nav d-flex flex-row">
           <li className="nav-item px-3">
             <Link className="nav-link" to="/">
-              Draft
+              Draft 
             </Link>
           </li>
           <li className="nav-item">
